bf57ad6b836cf0c9fddc634bb25a796018311ceb
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2025-02-12 23:14:32 -0500

    added corruption animation

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ let data = dataJSON;
 const s = ( p ) => {
     let font;
 
-    let canvasWidth = 1200;
-    let canvasHeight = 800;
+    let canvasWidth = window.innerWidth - 30;
+    let canvasHeight = window.innerHeight - 40;
     let backgroundColor = "rgb(200, 200, 200)";
     backgroundColor = "black";
     let mainColor = "rgb(100, 200, 200)";
@@ -22,8 +22,13 @@ const s = ( p ) => {
     subColor = "rgba(0, 123, 255, 0.89)";
     //subColor = "white;"
 
+    // Editable draw variables
     let scaleX = 0.5;
-    let scaleY = 1.4;
+    let scaleY = 1;
+
+    // Animation modes (turn on/off by button)
+    let modeScaleX = false;
+    let modeScaleY = false;
 
     let gapY = 0;
 
@@ -43,14 +48,54 @@ const s = ( p ) => {
         }
 
         // Clear canvas button
-        /*let clearCanvasBtn = p.createButton('Clear canvas');
-        clearCanvasBtn.position(700, 260);
-        clearCanvasBtn.mousePressed(() => {
-            // Reset canvas
-            p.clear();
-            //p.background(250);
-        });*/
+        let btn = p.createButton('-CORRUPT');
+        btn.position(canvasWidth - 70, 20);
+        btn.mousePressed(() => {
+            // turn on mode scale Y
+            modeScaleY = true;
+        });
+        btn.style("font-family", "Courier New");
+        btn.style("padding", "5px 10px");
+        btn.style("text-decoration", "underline");
+        btn.style("background-color", "black");
+        btn.style("color", "white");
+        btn.style("font-size", "12px");
 
+        let btn2 = p.createButton('-DESTROY');
+        btn2.position(canvasWidth - 70, 50);
+        btn2.mousePressed(() => {
+            // turn on mode scale Y
+            modeScaleX = true;
+        });
+        btn2.style("font-family", "Courier New");
+        btn2.style("padding", "5px 10px");
+        btn2.style("text-decoration", "underline");
+        btn2.style("background-color", "black");
+        btn2.style("color", "white");
+        btn2.style("font-size", "12px");
+
+        drawData();
+    }
+
+    // Scale function
+    function scaleTime(timeString, maxTime, maxScaled) {
+        let timeScaled = (Number(timeString.split(":")[0]) * 60 + Number(timeString.split(":")[1])) / maxTime * maxScaled;
+        return timeScaled;
+    }
+
+    p.draw = async () => {
+        // Animate based on mode
+        if (modeScaleX) {
+            scaleX += 0.1;
+            drawData();
+        }
+        if (modeScaleY) {
+            scaleY += 0.1;
+            drawData();
+        }
+    }
+
+    function drawData() {
         // Draw barcode from data
         let lineNum = 0; // for splitting days into lines vertically
         let lastDate = "";
@@ -86,7 +131,7 @@ const s = ( p ) => {
 
             // Calculate size and position based on time
             let width = lengthScaled * scaleX;
-            let height = 60 * scaleY;
+            let height = canvasHeight / 7 * scaleY;
             let posX = timeScaled * scaleX + 10;
             let posY = lineNum * height + gapY + 10; //10 + 4 * i;
 
@@ -109,17 +154,6 @@ const s = ( p ) => {
             }
         }
     }
-
-    // Scale function
-    function scaleTime(timeString, maxTime, maxScaled) {
-        let timeScaled = (Number(timeString.split(":")[0]) * 60 + Number(timeString.split(":")[1])) / maxTime * maxScaled;
-        return timeScaled;
-    }
-
-    p.draw = async () => {
-        
-    }
-
 }
 
 let mainCanvas = new p5(s);
