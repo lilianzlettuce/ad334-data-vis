70c3df47cf5cefeebc484f05ac122f9baef399d9
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2025-02-12 19:57:31 -0500

    added distinguishing colors, text

--- sketch.js
+++ sketch.js
@@ -7,21 +7,33 @@ const s = ( p ) => {
 
     let canvasWidth = 1200;
     let canvasHeight = 800;
-    let backgroundColor = "white";
-    let mainColor = "black";
+    let backgroundColor = "rgb(200, 200, 200)";
+    backgroundColor = "black";
+    let mainColor = "rgb(100, 200, 200)";
+    mainColor = "rgba(255, 0, 200, 0.87)";
+    mainColor = "rgba(0, 123, 255, 0.89)";
+    mainColor = "rgb(225, 225, 113)";
+    //mainColor = "white";
+
+    /*backgroundColor = "rgb(200, 200, 200)";
+    mainColor = "white";*/
+
     let subColor = "gray";
+    subColor = "rgba(0, 123, 255, 0.89)";
+    //subColor = "white;"
 
     let scaleX = 0.5;
-    let scaleY = 1;
+    let scaleY = 1.4;
 
-    let gapX = 0;
+    let gapY = 0;
 
     p.setup = () => {
         p.createCanvas(canvasWidth, canvasHeight);
+        p.blendMode(p.DIFFERENCE);
         p.background(backgroundColor);
         p.noStroke();
 
-        p.textSize(20);
+        p.textSize(10);
         p.textStyle(p.BOLD);
         if (font) {
             p.textFont(font);
@@ -39,6 +51,8 @@ const s = ( p ) => {
         });*/
 
         // Draw barcode from data
+        let lineNum = 0; // for splitting days into lines vertically
+        let lastDate = "";
         for (let i = 0; i < data.length; i++) {
             console.log(data[i])
             let obj = data[i];
@@ -49,7 +63,7 @@ const s = ( p ) => {
                 p.fill(mainColor);
             } else {
                 // Online
-                //p.fill(subColor);
+                p.fill(subColor);
             }
 
             // Process date-time strings
@@ -57,20 +71,32 @@ const s = ( p ) => {
             let time = obj.time.split("T")[1];
             let length = obj.length.split("-")[0];
 
+            // Update line number
+            if (i !== 0 && date !== lastDate) {
+                lineNum++;
+            }
+            lastDate = date;
+
             // Scale time and length to fit points in canvas
             let maxTime = 24 * 60; // in seconds
             let maxScaled = canvasWidth; // maximum x value to scale to
             let timeScaled = scaleTime(time, maxTime, maxScaled);
             let lengthScaled = scaleTime(length, maxTime, maxScaled);
 
-            // Calculate position based on time
-            let posX = timeScaled * scaleX;
-            let posY = 10 + 8 * i;
+            // Calculate size and position based on time
             let width = lengthScaled * scaleX;
-            let height = 20;
+            let height = 60 * scaleY;
+            let posX = timeScaled * scaleX + 10;
+            let posY = lineNum * height + gapY + 10; //10 + 4 * i;
 
             // x, y, width, height
             p.rect(posX, posY, width, height);
+
+            // Text label
+            p.textAlign(p.RIGHT, p.BOTTOM);
+            p.text(obj.name.substring(0, 2), posX, posY);
+            p.textAlign(p.RIGHT, p.BOTTOM);
+            p.text(obj.notes, posX + width, posY + height);
 
             if (obj.type === "P") {
                 // In person
@@ -78,14 +104,7 @@ const s = ( p ) => {
             } else {
                 // Online
                 // Draw line above
-                p.rect(posX, posY - 10, width, 4);
-                
-                // text creation
-                p.noStroke();
-                p.fill('black');
-                p.textSize(5);
-                p.textAlign(p.RIGHT, p.CENTER);
-                p.text(obj.name, posX, posY);
+                p.rect(posX, posY - 5, width, 2);
             }
 
             // x1, y1, x2, y2
